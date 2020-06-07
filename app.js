2b52ced27f2ec8a33638a57e61bfe4afd7be6aff
fabio.malagas@gmail.com <fabio.malagas@gmail.com>
2020-06-07 21:49:15 +0100

    Added minimal Softkeys handling implementation. Note: Replace window.alert

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+var keyLeft
+var keyCenter
+var keyRight
+
 window.addEventListener("load", function() {
   console.log("Hello World!");
 });
@@ -5,3 +9,40 @@ window.addEventListener("load", function() {
 function msg(){
   alert("Hello World!!");
 }
+
+
+/**
+* @brief KeyDown Handler
+*
+* @param et
+*
+* @return 
+*/
+
+
+
+function handleKeyDown(et) {
+	
+	switch (et.key)
+	{
+		case 'SoftLeft':
+			window.alert("Left softkey has been pressed");
+		break;
+
+		case 'Enter':
+			window.alert("Enter softkey has been pressed");
+		break;
+	
+		case 'SoftRight':
+			window.alert("Right softkey has been pressed");
+		break;
+
+	}		
+
+};
+
+
+document.addEventListener("keydown", handleKeyDown);
+
+
+
