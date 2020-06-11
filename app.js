7b1c3ab2f37f628b55406830d511905442cb5101
fabio.malagas@gmail.com <fabio.malagas@gmail.com>
2020-06-11 20:05:42 +0100

    Added input 'key' writing value on softkey event, to troubleshoot the Enter softkey..ongoing

--- app.js
+++ app.js
@@ -27,14 +27,20 @@ function handleKeyDown(et) {
 	{
 		case 'SoftLeft':
 			window.alert("Left softkey has been pressed");
+			document.getElementById("key-alert").value = "";
+			document.getElementById("key-alert").value = "SoftLeft";
 		break;
 
 		case 'Enter':
 			window.alert("Enter softkey has been pressed");
+			document.getElementById("key-alert").value = "";
+			document.getElementById("key-alert").value = "Enter";
 		break;
 	
 		case 'SoftRight':
 			window.alert("Right softkey has been pressed");
+			document.getElementById("key-alert").value = "";
+			document.getElementById("key-alert").value = "SoftRight"
 		break;
 
 	}		
